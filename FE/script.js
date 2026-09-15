001205fda6afe4c6f3be111ca5486b8f9d2715c2
BAVESH107 <baveshkatiger62@gmail.com>
2026-09-15 22:08:57 +0530

    Fix API_URL and connect to Render

--- FE/script.js
+++ FE/script.js
@@ -1,4 +1,4 @@
-const API_URL="https://question-paper-bnak.onrender.com";
+const API_URL="https://question-paper-bank.onrender.com";
 document.addEventListener('DOMContentLoaded', () => {
     loadAllPapers();
 });
